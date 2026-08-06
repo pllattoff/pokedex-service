aa49928aacf1d5ccbf06280a9a5b6e55c61c95e6
Stanislav Platov <pllattoff@gmail.com>
2026-08-06 11:16:45 +0200

    Initial commit

---
+++ src/main/java/org/example/pokedexservice/PokedexServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.pokedexservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/pokedexservice/PokedexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pokedexservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
